d646ce0648f66cf0bdc6d04845431bc5995f8764
Prakhar Mittal <mittalprakhar57@gmail.com>
2021-09-30 11:36:50 -0400

    Add paths and progress bar

--- src/main/java/com/example/towerdefense/controllers/GameController.java
+++ src/main/java/com/example/towerdefense/controllers/GameController.java
@@ -1,11 +1,11 @@
 package com.example.towerdefense.controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Screen;
@@ -24,8 +24,8 @@ public class GameController {
     private Tile[] tiles;
     private int[] tileImages;
 
-    private static int ROWS = 20;
-    private static int COLS = 35;
+    private static int ROWS = 40;
+    private static int COLS = 60;
 
     public void initialize() throws FileNotFoundException {
         int tileSize = (int) (Screen.getPrimary().getVisualBounds().getHeight() * 0.96 / ROWS);
@@ -37,12 +37,20 @@ public class GameController {
         for (int i = 0; i < tiles.length; i++) {
             int x = tileSize * (i % COLS);
             int y = tileSize * (i / COLS);
-            Tile tile = new Tile(x, y, false, tileSize, new Image("/" + tileImages[i] + ".jpg"));
+            Tile tile = new Tile(x, y, tileImages[i] != 0,
+                    tileSize, new Image("/" + tileImages[i] + ".jpg"));
             tiles[i] = tile;
             tile.setTranslateX(x);
             tile.setTranslateY(y);
             grid.getChildren().add(tile);
         }
+
+        ProgressBar monumentHealth = new ProgressBar();
+        monumentHealth.setProgress(1);
+        monumentHealth.setTranslateX(tileSize * 51);
+        monumentHealth.setTranslateY(tileSize * 14);
+        monumentHealth.setStyle("-fx-accent: #c90c22; -fx-background-insets: 0");
+        grid.getChildren().add(monumentHealth);
     }
 
     private int[] getTileImages() throws FileNotFoundException {
@@ -67,7 +75,8 @@ public class GameController {
             this.occupied = occupied;
             Rectangle border = new Rectangle(tileSize, tileSize);
             border.setFill(new ImagePattern(background));
-            border.setStroke(Color.DARKGREEN);
+            /*border.setStroke(Color.rgb(16, 16, 16));
+            border.setStrokeWidth(0.25);*/
             getChildren().add(border);
         }
     }
